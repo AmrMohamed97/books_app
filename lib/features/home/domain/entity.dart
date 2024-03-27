e82f18c2c8cd435577c690184af9c6437481c377
AmrMohamed97 <https://AmrMohamed97@github.com>
2024-03-27 06:41:44 +0200

    create entity

---
+++ lib/features/home/domain/entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  final String imageUrl;
+  final String title;
+  final String author;
+  final num price;
+  final num rating;
+
+  BookEntity({
+    required this.imageUrl,
+    required this.title,
+    required this.author,
+    required this.price,
+    required this.rating,
+  });
+}
